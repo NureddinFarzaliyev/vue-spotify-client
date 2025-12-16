2781e78fb7c6753cea60a0bfdf691833a1a39a0f
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-12-16 16:40:32 +0400

    minor ui fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth'
 import RightPanel from '@/widgets/RightPanel/RightPanel.vue'
+import classNames from 'classnames'
 import { RouterView } from 'vue-router'
 
 const auth = useAuthStore()
@@ -8,7 +9,12 @@ const auth = useAuthStore()
 
 <template>
   <div
-    class="h-dvh overflow-hidden grid grid-cols-1 lg:grid-cols-[2fr_1fr] xl:grid-cols-[3fr_1fr] bg-[#18181B]"
+    :class="
+      classNames(
+        'h-dvh overflow-hidden grid grid-cols-1 bg-[#18181B]',
+        auth.isLoggedIn && 'lg:grid-cols-[2fr_1fr] xl:grid-cols-[3fr_1fr]',
+      )
+    "
   >
     <div class="h-dvh overflow-scroll">
       <RouterView />
