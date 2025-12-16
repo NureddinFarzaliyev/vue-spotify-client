c6b6d4e5a65f3554f26a7711b79555f1f86bec65
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-12-16 08:44:26 +0400

    fix: empty playlist page

--- src/pages/playlist/Playlist.vue
+++ src/pages/playlist/Playlist.vue
@@ -20,7 +20,7 @@ onMounted(async () => {
 <template>
   <FullPageLoading v-if="loading" />
   <div v-else class="flex items-center flex-col gap-5 py-10 max-w-3xl mx-auto px-4">
-    <img :src="pl?.images[0]?.url" class="rounded-xl shadow-xl shadow-black/30" />
+    <img :src="pl?.images ? pl?.images[0]?.url : ''" class="rounded-xl shadow-xl shadow-black/30" />
     <h1 class="text-4xl mt-5 font-bold text-center">{{ pl?.name }}</h1>
     <p v-if="pl?.description" class="text-xl mb-2 opacity-80 text-center">{{ pl?.description }}</p>
     <i class="text-xl mb-5 opacity-80 text-center">{{ pl?.owner.display_name }}</i>
